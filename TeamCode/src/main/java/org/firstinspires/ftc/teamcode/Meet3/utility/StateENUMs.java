e27d48b728c51e2f775b567fd3cfced8be3a620f
birdyGoFly <kai@qualiaschool.org>
2023-12-08 10:14:21 -0800

    Beginning of Meet 3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Meet3/utility/StateENUMs.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Meet3.utility;
+
+public class StateENUMs {
+    public enum robotMode
+    {
+        intakePosition,
+        drivingPosition,
+        placementPosition,
+        //hangPosition,
+        //hangingPosition,
+
+    }
+}
