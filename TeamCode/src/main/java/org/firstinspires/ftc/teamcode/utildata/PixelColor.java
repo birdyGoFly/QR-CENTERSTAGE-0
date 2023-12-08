2dba37b7e88b82a725278e74a1ba8be50abdf700
birdyGoFly <kai@qualiaschool.org>
2023-12-07 19:03:56 -0800

    TeleOp from Meet 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utildata/PixelColor.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.utildata;
+
+public class PixelColor {
+
+    public enum PixelColors
+    {
+        WHITE,
+        GREEN,
+        PURPLE,
+        YELLOW,
+    }
+
+}
